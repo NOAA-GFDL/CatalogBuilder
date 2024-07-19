78897ff0fe24a0a48952b0308bffdf8b4ba5bf46
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-19 14:49:25 -0400

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,10 +1,26 @@
 def check_import():
  try:
-  from intakebuilder import getinfo, localcrawler, CSVwriter
-  print("Imported intakebuilder and getinfo, localcrawler, CSVwriter ")
- except ImportError:
-  raise ImportError('Error importing intakebuilder and other packages')
-  return -97
- return True
+   from intakebuilder import gfdlcrawler, CSVwriter, configparser
+   print("Imported intakebuilder, gfdlcrawler, CSVwriter, configparser")
+   return True
+ except ModuleNotFoundError:
+    print("The module intakebuilder is not installed. Do you have intakebuilder in your sys.path or have you activated the conda environment with the intakebuilder package in it? ")
+    print("Attempting again with adjusted sys.path ")
+    try:
+       sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    except:
+       print("Unable to adjust sys.path")
+    #print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    try:
+        from intakebuilder import gfdlcrawler, CSVwriter, builderconfig, configparser
+        print("Imported, and relied on sys.path")
+        return True
+    except ModuleNotFoundError:
+        sys.exit("The module 'intakebuilder' is still not installed. Do you have intakebuilder in your sys.path or have you activated the conda environment with the intakebuilder package in it? ")
+        raise ImportError('Error importing intakebuilder and other packages')
+        return -97
+     
 def test_import():
     assert check_import() == True
+
+
