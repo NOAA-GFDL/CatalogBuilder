d72f4064c86f4012576316290473f7e6f397140d
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-19 15:37:49 -0400

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,6 +6,7 @@ def check_import():
     print("The module intakebuilder is not installed. Do you have intakebuilder in your sys.path or have you activated the conda environment with the intakebuilder package in it? ")
     print("Attempting again with adjusted sys.path ")
     try:
+       #candobetter with actual conda package built before this, but this is a decent test for those that do not use conda package
        sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/catalogbuilder")
        from intakebuilder import gfdlcrawler, CSVwriter, builderconfig, configparser
     except:
