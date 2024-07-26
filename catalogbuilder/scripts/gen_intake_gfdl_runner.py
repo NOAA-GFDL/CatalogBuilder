2de966ab88f29188c3ace33eefd61fd3e5c00891
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-26 11:23:13 -0400

    Update gen_intake_gfdl_runner.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner.py
@@ -5,7 +5,7 @@ from catalogbuilder.scripts import gen_intake_gfdl
 #from . import gen_intake_gfdl
 import sys
 
-input_path = "/archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp/"
+input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
 output_path = "test"
 try:
   gen_intake_gfdl.create_catalog(input_path,output_path)
