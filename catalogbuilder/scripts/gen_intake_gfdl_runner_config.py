b82092fe7d2783c0dbc489c920a5648fb79b5968
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-29 16:32:40 -0400

    Update gen_intake_gfdl_runner_config.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
@@ -5,7 +5,7 @@ import sys
 
 #input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
 #output_path = "cftest"
-config = configs/config-example.yml
+config = "configs/config-example.yml"
 
 try:
   gen_intake_gfdl.create_catalog(configyaml=config)
