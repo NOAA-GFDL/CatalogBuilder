5a615768e2f94136b5bcc01dae03835a503870a0
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-29 16:16:18 -0400

    Update gen_intake_gfdl_runner.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner.py
@@ -2,7 +2,6 @@
 
 #TODO test after conda pkg is published and make changes as needed 
 from catalogbuilder.scripts import gen_intake_gfdl
-#from . import gen_intake_gfdl
 import sys
 
 input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
