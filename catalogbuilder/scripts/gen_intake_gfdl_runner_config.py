24537c8e9a98a56d261b49efd4a548c513822a2f
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-30 11:03:59 -0400

    Update gen_intake_gfdl_runner_config.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
@@ -5,8 +5,8 @@ import sys
 
 #input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
 #output_path = "cftest"
-config = "configs/config-example.yml"
-gen_intake_gfdl.create_catalog(configyaml=config)
+configyaml = "configs/config-example.yml"
+gen_intake_gfdl.create_catalog(config=configyaml)
 #except:
 #  sys.exit("Exception occured calling gen_intake_gfdl.create_catalog")
   
