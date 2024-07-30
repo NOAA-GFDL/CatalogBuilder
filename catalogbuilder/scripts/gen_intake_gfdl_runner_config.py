c78e7fbb882d6cade981da86400a0d6e3c107ea3
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-30 11:56:56 -0400

    Update gen_intake_gfdl_runner_config.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
@@ -4,7 +4,7 @@ from catalogbuilder.scripts import gen_intake_gfdl
 import sys, os 
 
 #This is an example call to run catalog builder using a yaml config file.
-
+package_dir = os.path.dirname(os.path.abspath(__file__))
 configyaml = os.path.join(package_dir, '../configs/config-example.yml')
 
 def create_catalog_from_config(config=configyaml):
