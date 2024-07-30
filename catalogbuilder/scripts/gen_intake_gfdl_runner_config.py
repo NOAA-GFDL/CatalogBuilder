fb796cfeb52e450bc91e94c6ec9904dd51cf5943
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-30 11:25:00 -0400

    Update gen_intake_gfdl_runner_config.py

--- catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
@@ -6,16 +6,7 @@ import sys
 #This is an example call to run catalog builder using a yaml config file.
 
 configyaml = os.path.join(package_dir, '../configs/config-example.yml')"
-gen_intake_gfdl.create_catalog(config=configyaml)
 
-#to output success/failure 
-
-csv = "/home/runner/work/forkCatalogBuilder-/sample-mdtf-catalog.csv"
-json = "/home/runner/work/forkCatalogBuilder-/sample-mdtf-catalog.json"
-
-csvpath = Path(csv)
-jsonpath = Path(json)
-
-assert csvpath.is_file()
-assert csvpath.is_file()
+def create_catalog_from_config(config=configyaml):
+    gen_intake_gfdl.create_catalog(config=configyaml)
 
