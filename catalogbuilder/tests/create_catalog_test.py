b076daec85e01966c624d6f24edd6d3f1429b148
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-30 10:33:57 -0400

    Update create_catalog_test.py

--- catalogbuilder/tests/create_catalog_test.py
+++ catalogbuilder/tests/create_catalog_test.py
@@ -10,11 +10,10 @@ def create_catalog(sample=True):
         chdir(Path(tmp))
         input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
         output_path = "test"
-        try:
-           json, csv = gen_intake_gfdl.create_catalog(input_path,output_path)
-        except:
-           sys.exit("Exception occured calling gen_intake_gfdl.create_catalog")
-        create_data_catalog(path)
+        #try:
+        json, csv = gen_intake_gfdl.create_catalog(input_path,output_path)
+       # except:
+      #     sys.exit("Exception occured calling gen_intake_gfdl.create_catalog")
         json, csv = Path(f"{output}.json").resolve(), Path(f"{output}.csv").resolve()
     
   assert not None in [csv,json]
