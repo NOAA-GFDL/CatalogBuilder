3f32518088f4fde3e7c44e6ac93c5d39979f27f2
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-30 12:00:36 -0400

    Update test_create_catalog.py

--- catalogbuilder/tests/test_create_catalog.py
+++ catalogbuilder/tests/test_create_catalog.py
@@ -1,7 +1,7 @@
 def test_create_catalog():
       import catalogbuilder
       from catalogbuilder.scripts import gen_intake_gfdl_runner_config
-      import make_sample_data
+      from catalogbuilder.tests import make_sample_data
       make_sample_data.make_sample_data()
       json, csv = gen_intake_gfdl_runner_config.create_catalog_from_config()
       #to output success/failure in pytest run with conda pkg local install in extra-tests CI workflow#
