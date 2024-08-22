bba4d0727628c3543bbcf5137c3cfd9d4eaec3b2
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-08-22 13:44:27 -0400

    Update setup.py
    version updated 2024.01.00

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='catalogbuilder',
-    version='2.0.1',
+    version='2024.01.01',
     packages=find_packages(),
     include_package_data=True,
     description='intake-esm Catalog Generation Utilities',
