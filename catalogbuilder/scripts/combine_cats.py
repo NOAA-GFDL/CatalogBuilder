d7ea030deae8e538829dbaf3c1288932e7b4eaf4
aradhakrishnanGFDL <aparna.radhakrishnan@noaa.gov>
2024-09-17 13:46:20 -0400

    diff json complete

--- catalogbuilder/scripts/combine_cats.py
+++ catalogbuilder/scripts/combine_cats.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import json
+from jsondiff import diff
 import pathlib
 import sys 
 import os
-#Set the csv files to be combined. The csv files here are the catalog files. 
+#Get the catalog files to be combined. 
 
-#csv1 = "/home/a1r/github/noaa-gfdl/catalogs/CM4.5v01_om5b06_piC_noBLING.csv"
+#USER INPUT BEGINS 
+
+#Pass the json catalog spec AND the combined_json which is the output path
+
 json1 = "/home/a1r/github/noaa-gfdl/catalogs/CM4.5v01_om5b06_piC_noBLING.json" 
+json2 = "/home/a1r/github/noaa-gfdl/catalogs/ESM4.5v01_om5b04_piC.json"
+combined_json = "/home/a1r/github/noaa-gfdl/catalogs/combined_CM4.5v01_om5b06_piC_noBLING_and_ESM4.5v01_om5b04_piC.json"
+
+#USER INPUT ENDS
 
 #Assume csv is in the same path and deduce the filename
 
@@ -14,7 +22,6 @@ p1 = pathlib.PurePath(json1)
 csv1 =  p1.with_suffix('.csv')
 print(csv1)
 
-json2 = "/home/a1r/github/noaa-gfdl/catalogs/ESM4.5v01_om5b04_piC.json"
 p2 = pathlib.Path(json2)
 csv2 = p2.with_suffix('.csv')
 print(csv2)
@@ -22,11 +29,20 @@ print(csv2)
 
 cat_csvs = [csv1,csv2] #TODO check for valid paths, pass it with cmd line if necessary 
 
-#####Check if the header of the csvs are the same #######
-
-
+#####Check if the schema is the same
+with open(json1) as f1, open(json2) as f2:
+    json_obj1 = json.load(f1)
+    json_obj2 = json.load(f2) 
+differ = diff(json_obj1, json_obj2) 
+print("INFO: Schema differs")
+print(differ)
+if len(differ.keys()) == 1:
+  if "catalog_file" in differ.keys():
+      print("We can combine since the catalog_file is the only difference")
+else:
+  print("Schema likely varies significantly, cannot combine")
+  sys.exit()
 #### If the headers are the same, append the data frames together and create the combined csv 
-combined_json = "/home/a1r/github/noaa-gfdl/catalogs/combined_CM4.5v01_om5b06_piC_noBLING_and_ESM4.5v01_om5b04_piC.json"
 p3 = pathlib.Path(combined_json)
 combined_csv = p3.with_suffix('.csv')
 
