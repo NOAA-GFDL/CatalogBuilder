54a8c9890cbaf685024f4a9ccdc3bdf87a47a23f
aradhakrishnanGFDL <aparna.radhakrishnan@noaa.gov>
2024-09-25 16:34:12 -0400

    ame output name as b4

--- catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
+++ catalogbuilder/scripts/gen_intake_gfdl_runner_config.py
@@ -6,7 +6,7 @@ import sys, os
 package_dir = os.path.dirname(os.path.abspath(__file__))
 configyaml = os.path.join(package_dir, 'configs/config-example.yml')
 input_path = "archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp"
-output_path = "sample-test"
+output_path = "sample-mdtf-catalog""
 
 def create_catalog_from_config(input_path=input_path,output_path=output_path,configyaml=configyaml):
     csv, json = gen_intake_gfdl.create_catalog(input_path=input_path,output_path=output_path,config=configyaml)
