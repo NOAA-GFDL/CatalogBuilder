678f3233d29f83126a847f3f6e8a79aefb1c0741
Ciheim <43519680+Ciheim@users.noreply.github.com>
2026-05-14 16:01:12 -0400

    Remove no-fill test and keep filled catalog coverage

--- catalogbuilder/tests/test_create_catalog.py
+++ catalogbuilder/tests/test_create_catalog.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import pandas as pd
 from catalogbuilder.scripts import gen_intake_gfdl_runner_config,gen_intake_gfdl_runner, make_sample_data
 
 def test_create_catalog():
       make_sample_data.make_sample_data()
-      json, csv = gen_intake_gfdl_runner_config.create_catalog_from_config()
+      csv, json = gen_intake_gfdl_runner_config.create_catalog_from_config()
       #to output success/failure in pytest run with conda pkg local install in extra-tests CI workflow#
       print(csv)
       csvpath = Path(csv)
@@ -11,7 +12,7 @@ def test_create_catalog():
       assert csvpath.is_file()
       assert jsonpath.is_file()
       #test to run without config so we can test the default configs/config_default.yaml
-      json, csv = gen_intake_gfdl_runner.create_catalog_default()
+      csv, json = gen_intake_gfdl_runner.create_catalog_default()
       #to output success/failure in pytest run with conda pkg local install in extra-tests CI workflow#
       print(csv)
       csvpath2 = Path(csv)
@@ -19,3 +20,7 @@ def test_create_catalog():
       assert csvpath2.is_file()
       assert jsonpath2.is_file()
 
+
+def test_create_catalog_fill():
+      df = pd.read_csv("default-mdtf-catalog.csv")
+      assert not df.isna().any().any()
